b90fae3ec9b553d0fea95765a0bd86b2c882a21a
unknown <ajmalbabu@gmail.com>
2019-02-17 19:36:00 -0600

    Remove producst

--- src/js/cms.js
+++ src/js/cms.js
@@ -3,7 +3,6 @@ import CMS from "netlify-cms";
 
 import HomePreview from "./cms-preview-templates/home";
 import PostPreview from "./cms-preview-templates/post";
-import ProductsPreview from "./cms-preview-templates/products";
 import ValuesPreview from "./cms-preview-templates/values";
 import ContactPreview from "./cms-preview-templates/contact";
 
